045490d922935e86f7ec72b29922a4ad4d1e154e
Rudrapratap Sahoo <rudrapratapsahoo04@gmail.com>
2025-12-26 08:58:18 +0530

    Fix MongoDB Atlas connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
+import mongoose from "mongoose"; 
+import dotenv from "dotenv";
 console.log("Server file loaded ✅");
 console.log("Server file loaded ✅");
 
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
 const app = express();
@@ -11,18 +14,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Music Backend Running 🎵");
+app.get("/test", (req, res) => {
+  res.send("Server & MongoDB working fine 🚀");
 });
+
 
 app.use("/songs", require("./routes/songRoutes"));
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("Mongo Error:", err));
+  .catch((err) => console.log("Mongo Error:", err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000 🚀");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
 
